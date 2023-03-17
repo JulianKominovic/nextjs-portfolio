7bf5d2dd4d9132d781065466ba712664f33700b5
Julian Kominovic <juliankominovic@gmail.com>
2023-03-16 23:38:53 -0300

    feat: seo & og tests

---
+++ public/metadata.ts
@@ -0,0 +1,15 @@
+export const COMMON_METADATA = {
+  title: "Julian E. Kominovic - Frontend Developer",
+  description:
+    "Julian Ezequiel Kominovic Frontend Developer at Koin. Blog, projects and more.",
+  viewport: "width=device-width, initial-scale=1",
+  keywords:
+    "Julian Kominovic, Julian Ezequiel Kominovic, Julian E. Kominovic, Julian, Kominovic, Frontend Developer, Frontend, Developer, Koin, Blog, Projects, Portfolio, Next.js, React, Typescript, Javascript, Javascript Developer, Typescript Developer, React Developer, Next.js Developer, Next.js Developer, Next.js, Next.js Blog, Next.js Portfolio",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    locale: "es_AR",
+    url: "https://jkominovic.vercel.app/",
+    countryName: "Argentina",
+  },
+};
