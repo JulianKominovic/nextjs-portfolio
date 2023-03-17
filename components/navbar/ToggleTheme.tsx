7a494678a2325748f99d7cbbbe5a3f39315782cf
Julian Kominovic <juliankominovic@gmail.com>
2023-03-16 21:49:31 -0300

    fix: localStorage is not defined

--- components/navbar/ToggleTheme.tsx
+++ components/navbar/ToggleTheme.tsx
@@ -5,13 +5,15 @@ import Sun from "@/assets/icons/Sun";
 import { useState } from "react";
 
 export default function ToggleTheme() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(
+    typeof window !== "undefined" ? localStorage.getItem("theme") : "light"
+  );
 
   return (
     <button
       className="flex items-center justify-center w-10 m-0 no-underline transition-colors rounded-full aspect-square hover:transition-colors hover:bg-neutral-300 dark:hover:bg-neutral-700 dark:text-white text-neutral-900"
       onClick={() => {
-        if (theme === "dark") {
+        if (theme === "dark" && typeof window !== "undefined") {
           document.documentElement.classList.remove("dark");
           setTheme("light");
           localStorage.setItem("theme", "light");
